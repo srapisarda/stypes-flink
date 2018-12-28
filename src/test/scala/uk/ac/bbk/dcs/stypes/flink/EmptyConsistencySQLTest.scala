5b09387b8bc775a6d456753f49a1216d13f84007
srapisarda <rapisarda.salvatore@gmail.com>
2018-12-28 09:38:44 +0000

    trying to optimise in  EmptyConsistencySQLTest.scala

--- src/test/scala/uk/ac/bbk/dcs/stypes/flink/EmptyConsistencySQLTest.scala
+++ src/test/scala/uk/ac/bbk/dcs/stypes/flink/EmptyConsistencySQLTest.scala
@@ -14,7 +14,7 @@ import org.scalatest.FunSpec
   */
 class EmptyConsistencySQLTest extends FunSpec with BaseFlinkTest {
   val calciteConfigBuilder = new CalciteConfigBuilder()
-  val ruleSets: RuleSet = RuleSets.ofList(JoinCommuteRule.INSTANCE, JoinAssociateRule.INSTANCE,
+  val ruleSets: RuleSet = RuleSets.ofList(
     LoptOptimizeJoinRule.INSTANCE)
   calciteConfigBuilder.addLogicalOptRuleSet(ruleSets)
   val tableConfig: TableConfig = new TableConfig()
@@ -25,8 +25,8 @@ class EmptyConsistencySQLTest extends FunSpec with BaseFlinkTest {
   private val fileNumber = 1
   tableEnv.registerTableSource("R", getDataSourceR(fileNumber))
   tableEnv.registerTableSource("S", getDataSourceS(fileNumber))
-  private lazy val r = tableEnv.scan("R")
-  private lazy val s = tableEnv.scan("S")
+  private val r = tableEnv.scan("R")
+  private val s = tableEnv.scan("S")
   //  private val rTableSet = tableEnv.toDataSet[Row](r)
   //  private val sTableSet = tableEnv.toDataSet[Row](s)
 
@@ -35,44 +35,35 @@ class EmptyConsistencySQLTest extends FunSpec with BaseFlinkTest {
 
     it("should read and execute the EmptyConsistencySQL for query q(x1,x4) <- R(x1,x2), R(x2,x3), S(x3,x4)") {
 
-      val table = r.as("x1, x2").join(r.as("x3, x4")).where("x2 === x3").select("x1, x4")
-        .join(s.as("x5, x6")).where("x4 === x5 ").select("x1, x6")
-
       val result = executeAsTable(1, "test", "empty-consistency", _ => {
         r.as("x1, x2").join(r.as("x3, x4")).where("x2 === x3").select("x1, x4")
-          .join(s.as("x5, x6")).where("x4 === x5 ").select("x1, x6")
+          .join(s.as("x5, x6")).where("x4 === x5").select("x1, x6")
       })
 
-      assert(result.count() == 0)
+      assert(result.first(1).count() == 0)
 
     }
 
     it("should read and execute the EmptyConsistencySQL  query for the sets {q(x1,x4) <- S(x1,x2), R(x2,x3), R(x3,x4)") {
       val result = executeAsTable(1, "test", "empty-consistency", _ => {
         s.as("x1, x2").join(r.as("x3, x4")).where("x2 === x3").select("x1, x4")
-          .join(r.as("x5, x6")).where("x4 === x5 ").select("x1, x6")
+          .join(r.as("x5, x6")).where("x4 === x5").select("x1, x6")
       })
 
-      assert(result.count() == 0)
+      assert(result.first(1).count() == 0)
 
     }
 
   }
 
 
-  private def executeAsTable(fileNumber: Int, serial: String, qName: String, f: Int => Table): DataSet[Row] = {
-    val startTime = System.nanoTime()
-    distinctTableSink(f.apply(fileNumber), fileNumber, serial, startTime, qName)
-  }
-
-  private def distinctTableSink(p1: Table, fileNumber: Int, serial: String, startTime: Long, qName: String): DataSet[Row] = {
+  private def executeAsTable(fileNumber: Int, serial: String, qName: String, f: Unit => Table): DataSet[Row] = {
+    val p1: Table = f.apply()
     println(tableEnv.explain(p1))
-    p1.groupBy("x1").select("x1.count")
     val count: Table = p1.groupBy("x1").select("x1 as cnt")
     val result: DataSet[Row] = tableEnv.toDataSet[Row](count)
-
     result
-
   }
+
 
 }
