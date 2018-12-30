1acb2106c5d32e3b3eba47ae1ee963ce4a6b0e5b
Salvatore Rapisarda <salvatore.rapisarda@camelotglobal.com>
2018-12-30 19:16:51 +0000

    the number of row in the table S it should be zero.

--- src/test/scala/uk/ac/bbk/dcs/stypes/flink/EmptyConsistencySQLTest.scala
+++ src/test/scala/uk/ac/bbk/dcs/stypes/flink/EmptyConsistencySQLTest.scala
@@ -1,13 +1,20 @@
 package uk.ac.bbk.dcs.stypes.flink
 
+import org.apache.calcite.rel.metadata.RelMetadataQuery
 import org.apache.calcite.rel.rules._
 import org.apache.calcite.tools.{RuleSet, RuleSets}
+import org.apache.flink.api.java.io.DiscardingOutputFormat
 import org.apache.flink.api.scala.{DataSet, _}
+import org.apache.flink.configuration.Configuration
+import org.apache.flink.optimizer.{DataStatistics, Optimizer}
 import org.apache.flink.table.api.scala.BatchTableEnvironment
 import org.apache.flink.table.api.{Table, TableConfig, TableEnvironment}
 import org.apache.flink.table.calcite.CalciteConfigBuilder
 import org.apache.flink.types.Row
 import org.scalatest.FunSpec
+
+import scala.util.Try
+
 
 /**
   * Created by salvo on 19/11/2018.
@@ -27,8 +34,18 @@ class EmptyConsistencySQLTest extends FunSpec with BaseFlinkTest {
   tableEnv.registerTableSource("S", getDataSourceS(fileNumber))
   private val r = tableEnv.scan("R")
   private val s = tableEnv.scan("S")
-  //  private val rTableSet = tableEnv.toDataSet[Row](r)
-  //  private val sTableSet = tableEnv.toDataSet[Row](s)
+
+
+  var relMetadataQuery = RelMetadataQuery.instance()
+
+  val rowCount = RelMetadataQuery.instance().getRowCount(s.getRelNode)
+  val sRowCount = s.getRelNode.estimateRowCount(RelMetadataQuery.instance())
+  //  (new JaninoRelMetadataProvider(DefaultRelMetadataProvider.INSTANCE), false))
+
+  private val rTableSet = tableEnv.toDataSet[Row](r)
+  private val sTableSet = tableEnv.toDataSet[Row](s)
+
+  private val qc = tableEnv.queryConfig
 
 
   describe("Flink SQL  Empty test") {
@@ -40,7 +57,7 @@ class EmptyConsistencySQLTest extends FunSpec with BaseFlinkTest {
           .join(s.as("x5, x6")).where("x4 === x5").select("x1, x6")
       })
 
-      assert(result.first(1).count() == 0)
+      assert(result.count() == 0)
 
     }
 
@@ -50,18 +67,53 @@ class EmptyConsistencySQLTest extends FunSpec with BaseFlinkTest {
           .join(r.as("x5, x6")).where("x4 === x5").select("x1, x6")
       })
 
-      assert(result.first(1).count() == 0)
+      assert(result.count() == 0)
 
     }
 
+    it("should assert 0 as row count for relation S") {
+      val actual = 0
+      val rowCount = RelMetadataQuery.instance().getRowCount(s.getRelNode)
+      assert(rowCount == actual)
+    }
+
+
+    it("should create a plan from an iteration") {
+      val test = Try {
+        val data = new DataStatistics()
+        val optimiser = new Optimizer(data, new Configuration())
+
+        optimiser.setDefaultParallelism(env.getParallelism)
+        val input: DataSet[Long] = env.generateSequence(1, 100)
+
+        val iteration = input.iterateWithTermination(100) {
+          previous =>
+            val next = previous.map { x => x + 1 }
+            val term = next.filter {
+              _ < 3
+            }
+            (next, term)
+        }
+
+        iteration.output(new DiscardingOutputFormat())
+
+        val plan = env.createProgramPlan()
+        val op = optimiser.compile(plan)
+
+        println(op)
+      }
+      if (test.isFailure) fail()
+
+    }
   }
 
 
-  private def executeAsTable(fileNumber: Int, serial: String, qName: String, f: Unit => Table): DataSet[Row] = {
-    val p1: Table = f.apply()
+  private def executeAsTable(fileNumber: Int, serial: String, qName: String, f: Unit => Table) = {
+    val p1 = f.apply()
     println(tableEnv.explain(p1))
-    val count: Table = p1.groupBy("x1").select("x1 as cnt")
-    val result: DataSet[Row] = tableEnv.toDataSet[Row](count)
+    val count = p1.groupBy("x1").select("x1 as cnt")
+    val result = tableEnv.toDataSet[Row](count)
+
     result
   }
 
