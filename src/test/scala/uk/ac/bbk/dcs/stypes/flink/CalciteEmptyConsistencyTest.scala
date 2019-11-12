e9b3bef2223850ec17d6b23fd69f8071d3b2d84b
srapisarda <rapisarda.salvatore@gmail.com>
2019-11-12 23:48:40 +0000

    test alignment with new ExternalCatalog

--- src/test/scala/uk/ac/bbk/dcs/stypes/flink/CalciteEmptyConsistencyTest.scala
+++ src/test/scala/uk/ac/bbk/dcs/stypes/flink/CalciteEmptyConsistencyTest.scala
@@ -2,11 +2,12 @@ package uk.ac.bbk.dcs.stypes.flink
 
 import org.apache.flink.api.scala._
 import org.apache.flink.table.api._
-import org.apache.flink.table.api.scala.{BatchTableEnvironment, _}
+import org.apache.flink.table.api.scala.BatchTableEnvironment
 import org.apache.flink.table.catalog._
+import org.apache.flink.table.catalog.stats.CatalogTableStatistics
 import org.apache.flink.table.descriptors._
 import org.apache.flink.types.Row
-import org.scalatest.{FunSpec, Matchers}
+import org.scalatest.{BeforeAndAfterAll, FunSpec, Matchers}
 
 import _root_.scala.collection.JavaConversions._
 
@@ -14,71 +15,154 @@ import _root_.scala.collection.JavaConversions._
 /**
   * Created by salvo on 19/11/2018.
   */
-class CalciteEmptyConsistencyTest extends FunSpec with BaseFlinkTest with Matchers {
-//  val calciteConfigBuilder = new CalciteConfigBuilder()
-//
-//  val ruleSets: RuleSet = RuleSets.ofList(
-//    LoptOptimizeJoinRule.INSTANCE)
-//  calciteConfigBuilder.addLogicalOptRuleSet(ruleSets)
-//  val tableConfig: TableConfig = new TableConfig()
-//  tableConfig.setCalciteConfig(calciteConfigBuilder.build())
-  private val tableEnv: BatchTableEnvironment = BatchTableEnvironment.create(env)
-  private val fileNumber = 1
-  private val model:String = "src/test/resources/benchmark/Lines/data/model"
+class CalciteEmptyConsistencyTest extends FunSpec with BaseFlinkTest with Matchers with BeforeAndAfterAll {
 
-  val catalogName = "S_CAT"
-  val databaseName = "S_EXT"
-  val tableName = "S"
+  private val bbSettings = EnvironmentSettings.newInstance().useBlinkPlanner().inBatchMode().build()
+  private val bbTableEnv = TableEnvironment.create(bbSettings)
+  private val tableEnv: BatchTableEnvironment = BatchTableEnvironment.create(env, bbTableEnv.getConfig)
+
+
+  private val catalogName = "S_CAT"
+  private val databaseName = "S_EXT"
+  private val tableNameS = "S"
+  private val tableNameA = "A"
+  private val tableNameR = "R"
+  private val pathS = new ObjectPath(databaseName, tableNameS)
+  private val pathA = new ObjectPath(databaseName, tableNameA)
+  private val pathR = new ObjectPath(databaseName, tableNameR)
+
+
+  private val catalog = getExternalCatalog(catalogName, 6)
+
+
+  override def beforeAll(): Unit = {
+    addStatisticsToCatalog()
+    tableEnv.registerCatalog(catalogName, catalog)
+    tableEnv.useCatalog(catalogName)
+    tableEnv.useDatabase(databaseName)
+    bbTableEnv.registerCatalog(catalogName, catalog)
+    bbTableEnv.useCatalog(catalogName)
+    bbTableEnv.useDatabase(databaseName)
+
+  }
+
+  override def afterAll(): Unit = {
+
+  }
 
   describe("Flink SQL  Empty test") {
 
     it("should assert 0 as row count for relation S") {
-      val catalog = getExternalCatalog(catalogName,1)
-      tableEnv.registerCatalog(catalogName, catalog)
-      tableEnv.useCatalog(catalogName)
-      tableEnv.useDatabase(databaseName)
-      val t: Table = tableEnv.scan(s"$tableName")
+      val t: Table = tableEnv.scan(s"$tableNameS")
       val rowCount = tableEnv.toDataSet[Row](t).count()
       val expected = 0
-//      val rowCount = 1 //RelMetadataQuery.instance().getRowCount(s1.)
       assert(rowCount == expected)
+    }
 
+    it("should assert 0 as row count for relation S using sql") {
+      val t: Table = tableEnv.sqlQuery("select * from S")
+      val rowCount = tableEnv.toDataSet[Row](t).count()
+      val expected = 0
+      assert(rowCount == expected)
+    }
+
+    it("should assert 0 as row count for relation R using sql") {
+      val t: Table = tableEnv.sqlQuery("select * from R")
+      val rowCount = tableEnv.toDataSet[Row](t).count()
+      val expected = 4101642
+      assert(rowCount == expected)
+    }
+
+    it("should assert 0 as row count for relation A using sql") {
+      val t: Table = tableEnv.sqlQuery("select * from A")
+      val rowCount = tableEnv.toDataSet[Row](t).count()
+      val expected = 492
+      assert(rowCount == expected)
+    }
+
+    it("should assert 0 as row count for query S inner join R using sql") {
+      val t: Table = tableEnv.sqlQuery("select * from S inner join R on S.Y=R.X")
+      val rowCount = tableEnv.toDataSet[Row](t).count()
+      val expected = 0
+      assert(rowCount == expected)
+    }
+
+    it("should assert 0 as row count for query S join R join A using sql") {
+      val t: Table = tableEnv.sqlQuery("select * from S inner join R on S.Y=R.X inner join A on R.Y=A.X")
+      val rowCount = tableEnv.toDataSet[Row](t).count()
+      val expected = 0
+      assert(rowCount == expected)
+    }
+
+    it("should assert 0 as row count for query R join R join A join S using sql") {
+      //      addStatisticsToCatalog()
+      val t: Table = tableEnv.sqlQuery("select r1.X, S.Y from R as r1 " +
+        "inner join R as r2 on r1.Y=r2.X inner join A on r2.Y=A.X " +
+        "inner join S on S.X=A.X")
+
+      val explanation = tableEnv.explain(false)
+      println(explanation)
+      val expected = 0
+      assert(0 == expected)
     }
 
   }
 
+
   def getExternalCatalog(catalogName: String, fileNumber: Int): Catalog = {
     val catalog = new GenericInMemoryCatalog(catalogName, databaseName)
-      // external Catalog table
-    val externalCatalogTableS = getExternalCatalogTable(tableName)
+    // external Catalog table
+    val externalCatalogTableS =  getExternalCatalogTable(tableNameS, fileNumber)
+    val externalCatalogTableA = getExternalCatalogTable(tableNameA, fileNumber)
+    val externalCatalogTableR = getExternalCatalogTable(tableNameR, fileNumber)
     // add external Catalog table
-    catalog.createTable( new ObjectPath( databaseName, tableName ), externalCatalogTableS, false)
+    catalog.createTable(pathS, externalCatalogTableS, false)
+    catalog.createTable(pathA, externalCatalogTableA, false)
+    catalog.createTable(pathR, externalCatalogTableR, false)
+
     catalog
   }
 
-  private def getTableSchema = {
-    TableSchema.builder()
-      .field("X", DataTypes.STRING)
-      .field("Y", DataTypes.STRING)
-      .build()
+  private def getTableSchema(filename: String) = {
+    val builder = TableSchema.builder()
+    builder.field("X", DataTypes.STRING)
+    if (filename != tableNameA)
+      builder.field("Y", DataTypes.STRING)
+
+    builder.build()
   }
 
-  private def getFileFormat =
-    new OldCsv().field("X", Types.STRING).field("Y", Types.STRING())
+  private def getFileFormat(fileName: String) = {
+    val csv = new OldCsv()
+    csv.field("X", Types.STRING)
+    if (fileName != tableNameA)
+      csv.field("Y", Types.STRING())
 
-  private def getExternalCatalogTable(fileName: String): CatalogBaseTable = {
+    csv
+  }
+
+  private def getExternalCatalogTable(fileName: String, fileNumber: Int): CatalogBaseTable = {
     val connectorDescriptor = new FileSystem()
-    val resourcePath = this.getClass.getResource(getFilePathAsResource(fileNumber, tableName)).getPath
+    val resourcePath = this.getClass.getResource(getFilePathAsResource(fileNumber, fileName)).getPath
     connectorDescriptor.path(resourcePath)
 
-    new CatalogTableBuilder(connectorDescriptor, getTableSchema)
-      .withFormat(getFileFormat)
+    new CatalogTableBuilder(connectorDescriptor, getTableSchema(fileName))
+      .withFormat(getFileFormat(fileName))
       .build()
 
+  }
+
+  private def addStatisticsToCatalog() = {
+    val tableStatisticsS = new CatalogTableStatistics(0, 1, 0, 0)
+    catalog.alterTableStatistics(pathS, tableStatisticsS, false)
+    val tableStatisticsR = new CatalogTableStatistics(4101642, 1, 46940747, 46940747)
+    catalog.alterTableStatistics(pathR, tableStatisticsR, false)
+    val tableStatisticsA = new CatalogTableStatistics(492, 1, 2807, 2807)
+    catalog.alterTableStatistics(pathA, tableStatisticsA, false)
   }
 
   private def getBatchTableProperties = {
-    val map = Map("is_streaming"->"false")
+    val map = Map("is_streaming" -> "false")
     mapAsJavaMap(map)
   }
 
